c3d56d007ec2524220be69b29074e0c9017adab1
jacky6632003 <jacky6632003@gmail.com>
2017-12-15 20:52:14 +0800

    加入專案檔案。

---
+++ WebApi/Models/Repository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+
+
+        private NorthwindEntities db = null;
+        private DbSet<T> Dbset = null;
+        public Repository()
+        {
+            db = new NorthwindEntities();
+            Dbset = db.Set<T>();
+        }
+
+
+        public void Create(T _entity)
+        {
+            Dbset.Add(_entity);
+            db.SaveChanges();
+        }
+
+        public void Delete(T _entity)
+        {
+            Dbset.Remove(_entity);
+            db.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return Dbset;
+        }
+
+        public T GetById(int id)
+        {
+            return Dbset.Find(id);
+        }
+
+        public void Update(T _entity)
+        {
+            db.Entry(_entity).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
