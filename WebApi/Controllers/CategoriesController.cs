6b73c2e8f0e178e2ea59bc6da2e230cc48b97d68
jacky6632003 <jacky6632003@gmail.com>
2017-12-18 13:33:32 +0800

    20171218

---
+++ WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class CategoriesController : ApiController
+    {
+
+
+        private Repository<Categories> repository = new Repository<Categories>();
+
+        public IQueryable<CategoriesViewModel> GetShippers(int id)
+        {
+
+            var q = from a in repository.GetAll()
+                    where a.CategoryID==id
+                    select new CategoriesViewModel
+                    {
+                      CategoryID=a.CategoryID,
+                      CategoryName=a.CategoryName,
+                      Description=a.Description,
+                      Picture= "/MVC/GetImage/"+ a.CategoryID,
+
+
+                    };
+
+            return q;
+        }
+
+    }
+}
--- WebApi/Controllers/MVCController.cs
+++ WebApi/Controllers/MVCController.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApi.Models;
 
 namespace WebApi.Controllers
 {
     public class MVCController : Controller
     {
         // GET: MVC
+        NorthwindEntities db = new NorthwindEntities();
         public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetImage(int id = 1)
+        {
+            Categories Categories = db.Categories.Find(id);
+            byte[] img = Categories.Picture;
+            if (img == null)
+            {
+                return File("~/images/NO.jpg", "image/png");
+            }
+            else
+                return File(img, "image/jpeg");
+        }
+
+
+        public ActionResult Index2()
         {
             return View();
         }
